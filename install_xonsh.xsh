2c18b73a2a29e5934579b7627787653c5a9ba252
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-03-20 19:21:36 +0000

    made changes for personal user.

---
+++ install_xonsh.xsh
@@ -0,0 +1,2 @@
+pip3.11 install xontrib-powerline2
+xpip install xontrib-xlsd
--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
+su -c "pip3.11 install 'xonsh[full]'" jackson
+su -c "pip3.11 install xontrib-powerline2" jackson
 pip3.11 install 'xonsh[full]'
+pip3.11 install xontrib-powerline2
 which xonsh >> /etc/shells
 chsh -s /usr/local/bin/xonsh jackson
+chmod +x .xonshrc
+rm /home/jackson/.xonshrc
+rm /home/jackson/install_xonsh.xsh
+cp .xonshrc /home/jackson/
+cp install_xonsh.xsh /home/jackson
 cd ./demo/ && php -S 127.0.0.1:3001&
 yarn start
